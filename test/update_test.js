93d0485667720223aa7703febad16a528f71ceaa
mong <dogcat614@gmail.com>
2017-01-04 14:29:00 +0800

    update mongodb with operator and validation

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updatting records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe'});
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(() => done());
   });
@@ -48,6 +48,15 @@ describe('Updatting records', () => {
       User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
       done
     );
+  });
+
+  it('A user can have their postcount incremented by 1', (done) => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
 
